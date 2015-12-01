e67918cef82e01db2bc28ee3af73ca276ab744dc
ryanzicky <514213431@qq.com>
2015-12-02 00:21:11 +0800

    First commit.

---
+++ app/src/main/java/com/coolweather/app/model/Province.java
@@ -0,0 +1,35 @@
+package com.coolweather.app.model;
+
+/**
+ * Created by Administrator on 2015/12/1 0001.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
+
